383246cc79a5fe6fe11b739655be16381651c732
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2021-04-26 18:19:53 +0530

    export in nodejs

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-const a = 4;
+const script2 = require('./script2.js'); 
+const a = script2.largeNumber;
 const b = 5;
+console.log(a + b);
 
-//GET'S THE DIRECTORY
-console.log(__dirname);
+/*GET'S THE DIRECTORY*/
+// console.log(__dirname);
 
 
 
