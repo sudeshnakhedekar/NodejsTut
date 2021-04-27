b4657f8a85b9b05a3cdd27f289887785c7fc55b3
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2021-04-27 19:47:42 +0530

    modules

---
+++ script3.js
@@ -0,0 +1,15 @@
+//build in modukes
+//most popular is 'fs' module
+
+/* TYPE 1 */
+// const c = require ('fs') ;
+
+// console.log(c);
+
+/*TYPE 2 */
+//used to build a severe
+
+const c = require('http');
+
+console.log(c);
+
